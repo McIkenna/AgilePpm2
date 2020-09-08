0c99b24264aba152788f6e206c4d827e6a731c11
sontoly <sontoly@gmail.com>
2020-09-08 13:12:26 -0400

    first commit

---
+++ ppm2/src/test/java/com/sonto/ppm2/Ppm2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonto.ppm2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ppm2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
